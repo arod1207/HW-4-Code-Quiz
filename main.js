788b2e34517cddfa7ffc81cdaf79d09b7b93a758
Arod1207 <arod1207@me.com>
2020-05-05 17:48:39 -0500

    Modified html and js

--- main.js
+++ main.js
@@ -1,72 +1,89 @@
-var startButton = document.querySelector(".start-btn");
-var button1 = document.createElement('button');
-var button2 =  document.createElement('button');
-var button3 =  document.createElement('button');
-var button4 =  document.createElement('button');
-var title = document.querySelector('.header-start');
-var answerButtons = document.querySelector('.answerButtons');
-var questions = document.querySelector('.questions');
-var question = ''
-var answer1 = '';
-var answer2 = '';
-var answer3 = '';
+var startBtn = document.querySelector('.startBtn');
+var quiz = document.querySelector('#quiz');
+var question = document.querySelector('#question');
+var counter = document.querySelector('#counter');
+var button1 = document.querySelector('#button1');
+var button2 = document.querySelector('#button2');
+var button3 = document.querySelector('#button3');
+var button4 = document.querySelector('#button4');
+var socre = document.querySelector('#score');
+var score = 0;
 
 
-
-
-startButton.addEventListener('click', startQuiz);
-
-//Starts the quiz,  it hides the title display and brings up the first question//
-function startQuiz(){
-   title.style.display = 'none';
-   button1.innerHTML = ('answer1');
-   button1.className = "btns";
-   document.body.appendChild(button1);
-   button2.innerHTML = ('answer2');
-   button2.className = "btns";
-   document.body.appendChild(button2);
-   button3.innerHTML = ('answer3');
-   button3.className = "btns";
-   document.body.appendChild(button3);
-//    questions.textContent = quizQuestions[0].question;
-   
-}
-// Quiz Questions in an array //
 var quizQuestions = [
     {
-        question: "What is the capital of Texas?",
-        answer: {
-            a: "San Antonio",
-            b: "Austin",
-            c: "Dallas",
-        },
-        correctAnswer: "b"
+        question : "This is question 1?",
+        answer1 : "Correct",
+        answer2 : "wrong",
+        answer3 : "Wrong",
+        answer4 : "Wrong",
+        correct : "A",
     },
     {
-        question: "What year was the Battle at the Alamo?",
-        answer: {
-            a: "1898",
-            b: "1803",
-            c: "1836",
-        },
-        correctAnswer: "c"
+        question : "This is question 2?",
+        answer1 : "Wrong",
+        answer2 : "Correct",
+        answer3 : "Wrong",
+        answer4 : "Wrong",
+        correWrong : "B",
     },
     {
-        question: "What is the state flower of Texas?",
-        answer: {
-            a: "Yellow Rose",
-            b: "Bluebonnet",
-            c: "Cactus",
-        },
-        correctAnswer: "b"
+        question : "This is question 3?",
+        answer1 : "Wrong",
+        answer2 : "Wrong",
+        answer3 : "Correct",
+        answer4 : "Wrong",
+        correWrong : "C",
+    },
+    {
+        question : "This is question 4?",
+        answer1 : "Wrong",
+        answer2 : "Wrong",
+        answer3 : "Wrong",
+        answer4 : "Correct",
+        correct : "D",
     },
 ]
 
- // loop through the area  for questions and answers //
-for (var i = 0; i < quizQuestions.length; i++){
-    if (quizQuestions[0] == "b"){
-        console.log('correct')
+
+
+var lastQuestion = quizQuestions.length - 1;
+var runningQuestion = 0;
+
+function renderQuestion(){
+    var questionsToAsk = quizQuestions[runningQuestion];
+
+    question.innerHTML = "<p>" + questionsToAsk.question + "</p>";
+    button1.innerHTML = questionsToAsk.answer1;
+    button2.innerHTML = questionsToAsk.answer2;
+    button3.innerHTML = questionsToAsk.answer3;
+    button4.innerHTML = questionsToAsk.answer4;
+
+    console.log(questionsToAsk.answer1)
+}
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz(){
+    startBtn.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+};
+
+// CHECK ANSWER //
+function checkAnswer(answer){
+    if(answer == quizQuestions[runningQuestion].correct){
+        score++
     } else {
-        console.log('wrong');
+         
     }
 }
+
+// function renderCounter() {
+//     let count = 0;
+//     var quizTime = 100; seconds
+//     if(count <= quiztime){
+//         counter.innerHTML = count
+//     }
+
+
